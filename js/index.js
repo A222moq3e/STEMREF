d4f647a05f7653ffab2462324985fd944fb57ff4
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2023-10-23 12:52:22 +0300

    add expressJs to project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-document.querySelector('button').addEventListener('click',()=>{
-    window.location.href='/pages/login.html'
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+// const htmlPath = path.join(__dirname,'../pages')
+// app.use(express.json())
+app.set("view engine","ejs")
+// app.set("views",htmlPath)
+// static files
+app.use(express.static('css'))
+
+app.get('/',(req,res)=>{
+    res.render("home")
+    // res.json({ error: err })
+
+})
+app.get('/login',(req,res)=>{
+    res.render("login")
+})
+
+
+app.listen(port,()=>{
+    console.log('port Connected in',`http://localhost:${port}`);
 })
