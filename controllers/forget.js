5924b27f141225750f851b47860ec601633c155a
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2024-01-16 22:02:22 +0300

    add forget page view

---
+++ controllers/forget.js
@@ -0,0 +1,9 @@
+// const Pages = require('../models/config');
+// const { usersCollection, coursesCollection } = require('../models/config');
+
+
+module.exports = {
+    forgetGet: (req,res)=>{
+        return res.render('forget')
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const router = express.Router();
 const controller = require('../controllers/pages')
+const controllerForget = require('../controllers/forget')
 console.log('in routes.js');
 router.use((req,res,next)=>{
     console.log('Time:', Date.now());
@@ -11,6 +12,7 @@ router.get('/login',controller.loginGet)
 router.post('/login',controller.loginPost)
 router.get('/register',controller.registerGet)
 router.post('/register',controller.registerPost)
+router.get('/forget',controllerForget.forgetGet)
 router.get('/signout',controller.signout)
 router.get('/search',controller.search)
 router.get('/profile',controller.profile)
