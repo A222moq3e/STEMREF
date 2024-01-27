e25469552e9d6564a7eb0e92fed781fb7d6563bb
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2024-01-27 19:13:32 +0300

    fix Cleartext Transmission of Sensitive Information (CWE-319)

--- js/index.js
+++ js/index.js
@@ -126,6 +126,7 @@ const httpsServer = https.createServer(httpsOptions, app);
 // const HTTP_PORT = 80;
 const HTTPS_PORT = 3443;
 
+
 // const HTTP_PORT = 3005;
 // const HTTPS_PORT = process.env.HTTPS_PORT || 3443;
 // const HTTP_PORT = process.env.HTTP_PORT || 3005;
