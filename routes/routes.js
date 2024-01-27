e09fad2d1fa17d681c64af6029ff6fbd35562d77
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2024-01-27 22:53:06 +0300

    fix Allocation of Resources Without Limits or Throttling (CWE-770)

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,19 @@ const express= require('express')
 const router = express.Router();
 const controller = require('../controllers/pages')
 const controllerForget = require('../controllers/forget')
+const rateLimit = require('express-rate-limit');
 console.log('in routes.js');
+// Rate Limit
+var limiter  = rateLimit({
+    windowMs: 10*60*1000, // 1 hour window
+    delayAfter: 10, // begin slowing down responses after the first 10 requests
+    delayMs: 100, // slow down subsequent responses by 100 milliseconds per request
+    max: 50, // start blocking after 50 requests
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: "Too many requests made from this IP, please try again in a few minutes"
+  });
+router.use(limiter)
 router.use((req,res,next)=>{
     console.log('Time:', Date.now());
     next()
