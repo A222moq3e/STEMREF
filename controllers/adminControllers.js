172bcc5957541a5766521054b5538c3a20c5e028
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2024-03-24 23:32:05 +0300

    add some comments

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -5,21 +5,31 @@ const { usersCollection, coursesCollection } = require('../models/config');
 
 module.exports = {
     adminGet:async (req,res)=>{
+        // Get admin Page
         const admin = new Admin(req.session.user.name,req.session.user.email)
         const users =  await usersCollection.find({})
         res.render('admin',{data:{user:admin,users:users,path:'/'+req.path.split('/')[1]}})
     },
     adminPost:(req,res)=>{
+        // For future data
         const admin = new Admin(req.session.user.name,req.session.user.email)
         // res.render('admin',{data:{user:admin}})
         res.status(403).send('nothing here')
     },
     adminPut:async (req,res)=>{
+        // Update User Data
         const admin = new Admin(req.session.user.name,req.session.user.email)
         console.log(`Update Successfully, ${req.params.user} to ${req.params.userType}`);
         const usersUpdate =  await usersCollection.updateOne({name:req.params.user},{userType:req.params.userType});
         // res.render('admin',{data:{user:admin,users:users}})
         res.send("update successfully")
+    },
+    adminSearchUsers: async (req,res)=>{
+        // Search for Users
+        const admin = new Admin(req.session.user.name,req.session.user.email)
+        const users =  await usersCollection.find({})
+        res.send('data serched')
+        // res.render('admin',{data:{user:admin,users:users,path:'/'+req.path.split('/')[1]}})
     }
 
 }
