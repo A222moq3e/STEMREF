2f48b09d72602c689d79ac1dc40a0d3986fbf676
abo3skr2019 <68832286+abo3skr2019@users.noreply.github.com>
2025-04-22 18:19:00 +0300

    Fixed Wrong env var in db.js XD

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGOURL || 'mongodb://localhost:27017/test');
+        await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/test');
         console.log('DB Connected Successfully');
     }
     catch (error) {
