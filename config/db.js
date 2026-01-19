38e6f405fde580c8b108a16df4d45f98b629ad7d
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-01-19 23:27:17 +0300

    index

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/test');
-        console.log('DB Connected Successfully');
-    }
-    catch (error) {
-        console.error('DB Connection Failed:', error);
-        process.exit(1); // Exit the process with failure
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "stemref" });
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.error("DB Connection Failed:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 // MISC
 const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 // i18next modules
 const i18next = require("i18next");
@@ -19,105 +20,108 @@ const path = require("path");
 const router = require("./routes/routes");
 const courseContentRoute = require("./routes/courseContentRoute");
 
-const secretSessionString = process.env.SECRET_SESSION || "thisisasecret";
+// check for environment variables
+required = ["SECRET_SESSION", "MONGO_URI", "BASE_URL", "MY_EMAIL_PASSWORD"];
+connectDB().then(() => {
+  const secretSessionString = process.env.SECRET_SESSION;
 
-const app = express();
+  const app = express();
 
-app.disable("x-powered-by");
+  app.disable("x-powered-by");
 
-// Connect to DB
-connectDB();
+  // Set EJS as templating engine
+  app.set("view engine", "ejs");
 
-// Set EJS as templating engine
-app.set("view engine", "ejs");
-
-// Add navbar Translator
-app.set("navsTranslator", {
-  Home: "/",
-  "Dashboard Admin": "/admin",
-  "Dashboard Educator": "/EducatorDashboardMain",
-  Search: "/search",
-  Pricing: "/pricing",
-  "Insert Course": "/EducatorDashboard",
-});
-
-// Static files middleware
-app.use(express.static(path.join(__dirname, "public")));
-
-i18next
-  .use(Backend)
-  .use(i18nextMiddleware.LanguageDetector)
-  .init({
-    debug: false,
-    fallbackLng: "en",
-    backend: {
-      loadPath: path.join(__dirname, "./locales/{{lng}}/translation.json"),
-    },
-    detection: {
-      order: ["querystring", "cookie"],
-      caches: ["cookie"],
-    },
+  // Add navbar Translator
+  app.set("navsTranslator", {
+    Home: "/",
+    "Dashboard Admin": "/admin",
+    "Dashboard Educator": "/EducatorDashboardMain",
+    Search: "/search",
+    Pricing: "/pricing",
+    "Insert Course": "/EducatorDashboard",
   });
-console.log(i18next.t("welcome"));
 
-app.use(i18nextMiddleware.handle(i18next));
+  // Static files middleware
+  app.use(express.static(path.join(__dirname, "public")));
+  // Rate Limit
+  const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour window
+    delayAfter: 20, // begin slowing down responses after the first 10 requests
+    delayMs: 100, // slow down subsequent responses by 100 milliseconds per request
+    max: 100, // start blocking after 50 requests
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message:
+      "Too many requests made from this IP, please try again in a few minutes",
+  });
 
-console.log("[+]", "process.env.TEST", process.env.TEST);
+  app.use(limiter);
 
-// Session (using MongoDB store for production)
-app.set("trust proxy", 1);
-app.use(
-  session({
-    secret: secretSessionString,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URI || "mongodb://localhost:27017/test",
-      collectionName: "sessions",
-      ttl: 14 * 24 * 60 * 60, // 14 days
+  i18next
+    .use(Backend)
+    .use(i18nextMiddleware.LanguageDetector)
+    .init({
+      debug: false,
+      fallbackLng: "en",
+      backend: {
+        loadPath: path.join(__dirname, "./locales/{{lng}}/translation.json"),
+      },
+      detection: {
+        order: ["querystring", "cookie"],
+        caches: ["cookie"],
+      },
+    });
+  console.log(i18next.t("welcome"));
+
+  app.use(i18nextMiddleware.handle(i18next));
+
+  console.log("[+]", "process.env.TEST", process.env.TEST);
+
+  // Session (using MongoDB store for production)
+  app.set("trust proxy", 1);
+  app.use(
+    session({
+      secret: secretSessionString,
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        client: mongoose.connection.getClient(),
+        dbname: "stemref",
+        collectionName: "sessions",
+        ttl: 14 * 24 * 60 * 60, // 14 days
+      }),
+      cookie: {
+        maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days
+        secure: process.env.NODE_ENV === "production", // only over HTTPS
+        httpOnly: true,
+        sameSite: "lax",
+      },
     }),
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
-      secure: process.env.NODE_ENV === "production", // only over HTTPS
-      httpOnly: true,
-      sameSite: "lax",
-    },
-  })
-);
+  );
 
-// Convert data into json
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+  // Convert data into json
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-// Rate Limit
-// const limiter = rateLimit({
-//   windowMs: 60 * 60 * 1000, // 1 hour window
-//   delayAfter: 20, // begin slowing down responses after the first 10 requests
-//   delayMs: 100, // slow down subsequent responses by 100 milliseconds per request
-//   max: 100, // start blocking after 50 requests
-//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-//   message:
-//     "Too many requests made from this IP, please try again in a few minutes",
-// });
+  app.use((req, res, next) => {
+    res.locals.req = req;
+    res.locals.navData = { access: false };
+    if (req.session?.user)
+      res.locals.navData = {
+        accesses: req.session.user.authenticated,
+        user: req.session.user,
+        path: "/" + req.path.split("/")[1],
+      };
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.locals.req = req;
-  res.locals.navData = { access: false };
-  if (req.session?.user)
-    res.locals.navData = {
-      accesses: req.session.user.authenticated,
-      user: req.session.user,
-      path: "/" + req.path.split("/")[1],
-    };
-  next();
-});
+  // Routing
+  app.use(router);
+  app.use(courseContentRoute);
 
-// Routing
-app.use(router);
-app.use(courseContentRoute);
-
-// Port Listner, [Do not remove this]
-app.listen(process.env.PORT || 3005, () => {
-  console.log("port Connected in", `http://localhost:3005`);
+  // Port Listner, [Do not remove this]
+  app.listen(process.env.PORT || 3005, () => {
+    console.log("port Connected in", `http://localhost:3005`);
+  });
 });
